89ff2f95662ade4018b299fcb07b4f6e7604f3f1
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2025-11-03 11:14:12 +0000

    feat: add memory allocation algorithm
    This commit adds a simple memory allocator. This algorithm allocates memory in page units (4KB / 4096 bytes).

--- kernel.c
+++ kernel.c
@@ -29,6 +29,21 @@ void putchar(char ch) {
   sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
 }
 
+extern char __free_ram[], __free_ram_end[];
+
+// memory allocation algorithm
+paddr_t alloc_pages(uint32_t n) {
+    static paddr_t next_paddr = (paddr_t) __free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t) __free_ram_end)
+        PANIC("out of memory");
+
+    memset((void *) paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
+
 // entry point of exception handler
 __attribute__((naked))
 __attribute__((aligned(4)))
@@ -126,6 +141,12 @@ void kernel_main(void) {
 
   printf("\n\nSuccessful boot of BasicOS.\n");
   // printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+
+  // test memory allocation algorithm
+  // paddr_t paddr0 = alloc_pages(2);
+  // paddr_t paddr1 = alloc_pages(1);
+  // printf("alloc_pages test: paddr0=%x\n", paddr0);
+  // printf("alloc_pages test: paddr1=%x\n", paddr1);
 
   for (;;) {
     __asm__ __volatile__("wfi");
