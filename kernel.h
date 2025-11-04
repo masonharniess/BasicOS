6d660b3ba1cd8a644b83aaa8d9857e637fcea619
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2025-11-04 21:15:46 +0000

    refactor: improve kernel.h structure
    This commit restructures the code in kernel.h to be more readable and removes any redundant code.

--- kernel.h
+++ kernel.h
@@ -1,74 +1,22 @@
-#include "common.h"
 #pragma once
-#define SSTATUS_SUM  (1 << 18)
-#define FILES_MAX      2
-#define DISK_MAX_SIZE  align_up(sizeof(struct file) * FILES_MAX, SECTOR_SIZE)
+#include "common.h"
 
-struct tar_header {
-    char name[100];
-    char mode[8];
-    char uid[8];
-    char gid[8];
-    char size[12];
-    char mtime[12];
-    char checksum[8];
-    char type;
-    char linkname[100];
-    char magic[6];
-    char version[2];
-    char uname[32];
-    char gname[32];
-    char devmajor[8];
-    char devminor[8];
-    char prefix[155];
-    char padding[12];
-    char data[];      // array pointing to the data area following the header
-                      // (flexible array member)
-} __attribute__((packed));
-
-struct file {
-    bool in_use;      // indicates if this file entry is in use
-    char name[100];   // file name
-    char data[1024];  // file content
-    size_t size;      // file size
-};
-
-#define SSTATUS_SPIE (1 << 5)
+#define PROCS_MAX 8
+#define PROC_UNUSED   0
+#define PROC_RUNNABLE 1
 #define PROC_EXITED   2
-#define PANIC(fmt, ...)                                                        \
-    do {                                                                       \
-        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
-        while (1) {}                                                           \
-    } while (0)
-
-// macro for reading CSR registers
-#define READ_CSR(reg)                                                          \
-    ({                                                                         \
-        unsigned long __tmp;                                                   \
-        __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp));                  \
-        __tmp;                                                                 \
-    })
-
-// macro for writing CSR registers
-#define WRITE_CSR(reg, value)                                                  \
-    do {                                                                       \
-        uint32_t __tmp = (value);                                              \
-        __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                \
-    } while (0)
-
-#define PROCS_MAX 8       // maximum number of processes
-
-#define PROC_UNUSED   0   // unused process control structure
-#define PROC_RUNNABLE 1   // runnable process
-
-// page table
-#define SATP_SV32 (1u << 31) // A single bit in the satp register, indicates to enable paging in SV32 mode
-#define PAGE_V    (1 << 0)   // "valid" bit (entry is enabled)
-#define PAGE_R    (1 << 1)   // readable
-#define PAGE_W    (1 << 2)   // writable
-#define PAGE_X    (1 << 3)   // executable
-#define PAGE_U    (1 << 4)   // user (accessible in user mode)
-
+#define SATP_SV32 (1u << 31)
+#define SSTATUS_SPIE (1 << 5)
+#define SSTATUS_SUM  (1 << 18)
+#define SCAUSE_ECALL 8
+#define PAGE_V    (1 << 0)
+#define PAGE_R    (1 << 1)
+#define PAGE_W    (1 << 2)
+#define PAGE_X    (1 << 3)
+#define PAGE_U    (1 << 4)
+#define USER_BASE 0x1000000
+#define FILES_MAX   2
+#define DISK_MAX_SIZE     align_up(sizeof(struct file) * FILES_MAX, SECTOR_SIZE)
 #define SECTOR_SIZE       512
 #define VIRTQ_ENTRY_NUM   16
 #define VIRTIO_DEVICE_BLK 2
@@ -81,7 +29,6 @@ struct file {
 #define VIRTIO_REG_QUEUE_NUM     0x38
 #define VIRTIO_REG_QUEUE_ALIGN   0x3c
 #define VIRTIO_REG_QUEUE_PFN     0x40
-#define VIRTIO_REG_QUEUE_READY   0x44
 #define VIRTIO_REG_QUEUE_NOTIFY  0x50
 #define VIRTIO_REG_DEVICE_STATUS 0x70
 #define VIRTIO_REG_DEVICE_CONFIG 0x100
@@ -95,23 +42,52 @@ struct file {
 #define VIRTIO_BLK_T_IN  0
 #define VIRTIO_BLK_T_OUT 1
 
-// the base virtual address of an application image. This needs to match the starting address defined in `user.ld`.
-#define USER_BASE 0x1000000
-
-#define SCAUSE_ECALL 8
-
 struct process {
-    int pid;              // process ID
-    int state;            // process state: PROC_UNUSED or PROC_RUNNABLE
-    vaddr_t sp;           // stack pointer
-    uint32_t *page_table; //pointer to first-level page table
-    uint8_t stack[8192];  // kernel stack
+    int pid; 
+    int state; 
+    vaddr_t sp; 
+    uint32_t *page_table; 
+    uint8_t stack[8192];
 };
 
 struct sbiret {
-  long error;
-  long value;
+    long error;
+    long value;
 };
+
+struct trap_frame {
+    uint32_t ra;
+    uint32_t gp;
+    uint32_t tp;
+    uint32_t t0;
+    uint32_t t1;
+    uint32_t t2;
+    uint32_t t3;
+    uint32_t t4;
+    uint32_t t5;
+    uint32_t t6;
+    uint32_t a0;
+    uint32_t a1;
+    uint32_t a2;
+    uint32_t a3;
+    uint32_t a4;
+    uint32_t a5;
+    uint32_t a6;
+    uint32_t a7;
+    uint32_t s0;
+    uint32_t s1;
+    uint32_t s2;
+    uint32_t s3;
+    uint32_t s4;
+    uint32_t s5;
+    uint32_t s6;
+    uint32_t s7;
+    uint32_t s8;
+    uint32_t s9;
+    uint32_t s10;
+    uint32_t s11;
+    uint32_t sp;
+} __attribute__((packed));
 
 struct virtq_desc {
     uint64_t addr;
@@ -154,37 +130,49 @@ struct virtio_blk_req {
     uint8_t status;
 } __attribute__((packed));
 
-// represents the program state saved in kernel_entry
-struct trap_frame {
-    uint32_t ra;
-    uint32_t gp;
-    uint32_t tp;
-    uint32_t t0;
-    uint32_t t1;
-    uint32_t t2;
-    uint32_t t3;
-    uint32_t t4;
-    uint32_t t5;
-    uint32_t t6;
-    uint32_t a0;
-    uint32_t a1;
-    uint32_t a2;
-    uint32_t a3;
-    uint32_t a4;
-    uint32_t a5;
-    uint32_t a6;
-    uint32_t a7;
-    uint32_t s0;
-    uint32_t s1;
-    uint32_t s2;
-    uint32_t s3;
-    uint32_t s4;
-    uint32_t s5;
-    uint32_t s6;
-    uint32_t s7;
-    uint32_t s8;
-    uint32_t s9;
-    uint32_t s10;
-    uint32_t s11;
-    uint32_t sp;
+struct tar_header {
+    char name[100];
+    char mode[8];
+    char uid[8];
+    char gid[8];
+    char size[12];
+    char mtime[12];
+    char checksum[8];
+    char type;
+    char linkname[100];
+    char magic[6];
+    char version[2];
+    char uname[32];
+    char gname[32];
+    char devmajor[8];
+    char devminor[8];
+    char prefix[155];
+    char padding[12];
+    char data[];
 } __attribute__((packed));
+
+struct file {
+    bool in_use;
+    char name[100];
+    char data[1024];
+    size_t size;
+};
+
+#define READ_CSR(reg)                                                          \
+    ({                                                                         \
+        unsigned long __tmp;                                                   \
+        __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp));                  \
+        __tmp;                                                                 \
+    })
+
+#define WRITE_CSR(reg, value)                                                  \
+    do {                                                                       \
+        uint32_t __tmp = (value);                                              \
+        __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                \
+    } while (0)
+
+#define PANIC(fmt, ...)                                                        \
+    do {                                                                       \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
+        while (1) {}                                                           \
+    } while (0)
